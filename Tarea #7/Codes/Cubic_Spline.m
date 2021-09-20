b4ad5db377d0d4da5ca2dcf5b8147626c190d724
alexanyernas <alexanyernaranjo@gmail.com>
2021-09-20 16:22:00 -0400

    Tarea #7 - Sources Codes

--- Tarea #7/Codes/Cubic_Spline.m
+++ Tarea #7/Codes/Cubic_Spline.m
@@ -1,4 +1,10 @@
-function cubic_driver(num_points)
+% Estudiante: Naranjo Sthory Alexanyer Antonio
+% Cedula de identidad: V - 26.498.600
+% Asignatura: Calculo Cientifico (6105)
+% Tarea 7: Interpolacion polinomial y Splines
+% Spline Cubico Natural Interpolante
+
+function cubic_driver
   %  cubic_driver(n) computes a cubic spline
   %  interpolant of the runge function
   %    
@@ -8,18 +14,13 @@ function cubic_driver(num_points)
   %  points in the interval [-1,1] 
 
   runge = @(x) 1./(1+ 25*x.^2);
-  
   num_points = 5;
-
   x = linspace(-1,1,num_points);
   y = runge(x);
-
   [s0,s1,s2,s3] = cubic_spline(x',y');
-
   plot_points = 1000;
   xx = linspace(-1,1,plot_points);
   yy = runge(xx);
-
   plot(xx,yy,'g');
   hold on;
   plot_cubic_spline(x,s0,s1,s2,s3);
@@ -48,26 +49,18 @@ function [s0,s1,s2,s3]=cubic_spline(x,y)
   if any(size(x) ~= size(y)) || size(x,2) ~= 1
       error('inputs x and y must be column vectors of equal length');
   end
-
-  n = length(x)
-
+  n = length(x);
   h = x(2:n) - x(1:n-1);
   d = (y(2:n) - y(1:n-1))./h;
-
   lower = h(1:end-1);
   main  = 2*(h(1:end-1) + h(2:end));
   upper = h(2:end);
-
   T = spdiags([lower main upper], [-1 0 1], n-2, n-2);
   rhs = 6*(d(2:end)-d(1:end-1));
-
   m = T\rhs;
-
   % Use natural boundary conditions where second derivative
   % is zero at the endpoints
-
   m = [ 0; m; 0];
-
   s0 = y;
   s1 = d - h.*(2*m(1:end-1) + m(2:end))/6;
   s2 = m/2;
@@ -81,9 +74,7 @@ function plot_cubic_spline(x,s0,s1,s2,s3)
   % and coefficents s0, s1, s2, s3
 
   n = length(x);
-
   inner_points = 20;
-
   for i=1:n-1
       xx = linspace(x(i),x(i+1),inner_points);
       xi = repmat(x(i),1,inner_points);
@@ -93,3 +84,5 @@ function plot_cubic_spline(x,s0,s1,s2,s3)
       plot(x(i),0,'r');
   end
 endfunction
+
+cubic_driver();
--- Tarea #7/Codes/Newton_Interpolation.m
+++ Tarea #7/Codes/Newton_Interpolation.m
@@ -1,40 +1,48 @@
-clc; %Clears editor screen and background
-%clf; %Clears the plot screen 
-format long; %sets format to long
+% Estudiante: Naranjo Sthory Alexanyer Antonio
+% Cedula de identidad: V - 26.498.600
+% Asignatura: Calculo Cientifico (6105)
+% Tarea 7: Interpolacion polinomial y Splines
+% Polinomio Interpolante de grado menor o igual a n
 
+close all
+clear all
+clc
+format long
 
-printf("Newton's Interpolation method\n");
-n=input("Enter the number of data points:");
-%X=input("Enter x");
-X=linspace(2,10,n);
-Y=log(X.^1);
-DD(1:n,1)=X;
-DD(1:n,2)=Y;
+function newton_interpolation
+  n = 10;
+  X = linspace(2,10,n);
+  Y = log(X.^1);
+  DD(1:n,1) = X;
+  DD(1:n,2) = Y;
 
-for j = 1:n-1
- for k = 1:n-j  
- DD(k,j+2) = (DD(k+1,j+1)-DD(k,j+1))/(DD(k+j,1)-DD(k,1));
- end
-end
+  for j = 1:n-1
+   for k = 1:n-j  
+   DD(k,j+2) = (DD(k+1,j+1)-DD(k,j+1))/(DD(k+j,1)-DD(k,1));
+   end
+  end
 
-Prod = zeros(n-1,n); 
-p = conv(1,[1,-X(1)]); %  poly (x-x(1))
- 
-for i = 1:n-1
- Prod(i,(n-(length(p)-1)):n) = DD(1,i+2)*p;
- p = conv(p,[1,-X(i+1)]); % poly (x-x(1)*(x-x(2))....
-end
- 
-P = sum(Prod,1);
-P(1,n)=P(1,n)+Y(1);
- 
-e= log(9.5) - polyval(P,9.5)
+  Prod = zeros(n-1,n); 
+  p = conv(1,[1,-X(1)]); %  poly (x-x(1))
+   
+  for i = 1:n-1
+   Prod(i,(n-(length(p)-1)):n) = DD(1,i+2)*p;
+   p = conv(p,[1,-X(i+1)]); % poly (x-x(1)*(x-x(2))....
+  end
+   
+  P = sum(Prod,1);
+  P(1,n) = P(1,n)+Y(1);
+   
+  e = log(9.5) - polyval(P,9.5)
 
-x_eval = min(X):0.1:max(X);
-y_eval = polyval(P,x_eval);
-plot(x_eval,y_eval); 
-hold on;
-plot(X,Y,"xr");
-hold on;
-plot(X,log(X),"or")
-Coefficients = P;
+  x_eval = min(X):0.1:max(X);
+  y_eval = polyval(P,x_eval);
+  plot(x_eval,y_eval); 
+  hold on;
+  plot(X,Y,"xr");
+  hold on;
+  plot(X,log(X),"or")
+  Coefficients = P;
+endfunction
+
+newton_interpolation();
